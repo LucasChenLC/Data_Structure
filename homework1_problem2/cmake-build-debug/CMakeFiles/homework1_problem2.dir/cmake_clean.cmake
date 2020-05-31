3373e390c42c55f74ac6643d61fda92dfbe19a44
CJX32 <meteorskysun@outlook.com>
2020-06-01 00:03:40 +0800

    bug fixed

---
+++ homework1_problem2/cmake-build-debug/CMakeFiles/homework1_problem2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework1_problem2.dir/main.cpp.o"
+  "homework1_problem2"
+  "homework1_problem2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework1_problem2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework3_problem1/cmake-build-debug/CMakeFiles/homework3_problem1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework3_problem1.dir/main.cpp.o"
+  "homework3_problem1"
+  "homework3_problem1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework3_problem1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
